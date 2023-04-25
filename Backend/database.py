794e751110e77adac927f31c0d99aa7d7f9f6268
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-04-25 18:39:49 -0300

    Limpado Frontend e atualizado Backend
    Limpei os arquivos inúteis do Frontend para começar a codar, atualizei o Backend para começar a usar o SQLalchemy ao em vez do psycopg2.

--- Backend/database.py
+++ Backend/database.py
@@ -1,36 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 from dotenv import load_dotenv
 import os
-import psycopg2
 
 load_dotenv()
-
-# Função para criar conexão no banco
-def conectar():
-  con = psycopg2.connect(host=os.environ["host"], 
-                         database=os.environ["database"],
-                         user=os.environ["user"], 
-                         password=os.environ["password"])
-  return con
-
-# Função para inserir dados no banco
-def inserir(sql):
-    con = conectar()
-    cur = con.cursor()
-    try:
-        cur.execute(sql)
-        con.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        con.rollback()
-        cur.close()
-        return 1
-    cur.close()
-
-# Função para consultas no banco
-def consultar(sql):
-  con = conectar()
-  cur = con.cursor()
-  cur.execute(sql)
-  recset = cur.fetchall()
-  con.close()
-  return recset
+host=os.environ["host"] 
+database=os.environ["database"]
+user=os.environ["user"],
+password=os.environ["password"]
+  
+engine = create_engine(f'postgresql+psycopg2://postgres:postgres@{host}/{database}',isolation_level="SERIALIZABLE",)
+print(engine)
