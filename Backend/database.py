2541c77aaf5075e530eb5c6a6690862c8ec5c91d
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-04-25 20:35:32 -0300

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
 
@@ -10,5 +11,21 @@ database=os.environ["database"]
 user=os.environ["user"],
 password=os.environ["password"]
   
-engine = create_engine(f'postgresql+psycopg2://postgres:postgres@{host}/{database}',isolation_level="SERIALIZABLE",)
-print(engine)
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/baseprojetopessoal')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class Pessoas(Base):
+    __tablename__ = "pessoas"
+
+    name: Column(String, nullable=False)
+    age: Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Pessoa (Nome:{self.name}, Idade:{self.age})"
+    
+#Select
+data = session.query(Pessoas).all()
+print(data)
