b4431676ba30748ddc0fdf40cc5dd274cf4939b8
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-05-04 21:21:42 -0300

    criado app django para começar o backend

--- Backend/peoples/models.py
+++ Backend/peoples/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    NIVEL = (
+        ('G', 'Gold'),
+        ('P', 'Platinum'),
+        ('I', 'Iron')
+    )
+    nome: models.CharField(max_length=30, null=False, blank=False)
+    sobrenome: models.CharField(max_length=60, null=False, blank=False)
+    email: models.EmailField(max_length=100, unique=True, null=False, blank=False)
+    data_nascimento: models.DateField(blank=False, null=False)
+    tipo_cliente: models.CharField(max_length=1, null=False, blank=False, choices=NIVEL, default='I')
+
+    def __str__(self):
+        return self.nome + '' + self.sobrenome
