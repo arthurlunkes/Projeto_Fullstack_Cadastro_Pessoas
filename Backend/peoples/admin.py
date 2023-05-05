d148b77579e4a64ae49dbf60a5691e1370c194ac
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-05-05 15:34:53 -0300

    Inserido o model Cliente no admin.py

--- Backend/peoples/admin.py
+++ Backend/peoples/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+class Cliente(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'email', 'data_nascimento', 'tipo_cliente')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
